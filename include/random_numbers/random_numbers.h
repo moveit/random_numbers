0108aed549adbb956e552a9f0e1148a344b52de8
Robert Haschke <rhaschke@users.noreply.github.com>
2019-03-28 10:24:43 +0100

    Update Travis (#16)
    * make Travis use moveit_ci
    * clang-format
    * enable catkin_lint check

--- include/random_numbers/random_numbers.h
+++ include/random_numbers/random_numbers.h
@@ -54,19 +54,18 @@ namespace random_numbers
 class RandomNumberGenerator
 {
 public:
-  
   /** \brief Constructor. Always sets a "random" random seed */
   RandomNumberGenerator(void);
 
   /** \brief Constructor. Allow a seed to be specified for deterministic behaviour */
   RandomNumberGenerator(boost::uint32_t seed);
-  
+
   /** \brief Generate a random real between 0 and 1 */
   double uniform01(void)
   {
     return uni_();
   }
-  
+
   /**
    * @brief Generate a random real within given bounds: [\e lower_bound, \e upper_bound)
    * @param lower_bound The lower bound
@@ -82,39 +81,38 @@ public:
   {
     return normal_();
   }
-  
+
   /** \brief Generate a random real using a normal distribution with given mean and variance */
   double gaussian(double mean, double stddev)
   {
     return normal_() * stddev + mean;
   }
-  
+
   /** \brief Uniform random unit quaternion sampling. The computed value has the order (x,y,z,w)
    * @param value[4] A four dimensional array in which the computed quaternion will be returned
    */
   void quaternion(double value[4]);
-  
+
   /** \brief Generate an integer uniformly at random within a specified range (inclusive) */
   int uniformInteger(int min, int max)
   {
-    boost::uniform_int<> dis(min, max);  
+    boost::uniform_int<> dis(min, max);
     return dis(generator_);
   }
 
   /**
-   * \brief Allow the randomly generated seed to be saved so that experiments / benchmarks can be recreated in the future
+   * \brief Allow the randomly generated seed to be saved so that experiments / benchmarks can be recreated in the
+   * future
    */
   boost::uint32_t getFirstSeed();
-  
+
 private:
-  
-  boost::mt19937                                                           generator_;
-  boost::uniform_real<>                                                    uniDist_; 
-  boost::normal_distribution<>                                             normalDist_;
-  boost::variate_generator<boost::mt19937&, boost::uniform_real<> >        uni_;  
+  boost::mt19937 generator_;
+  boost::uniform_real<> uniDist_;
+  boost::normal_distribution<> normalDist_;
+  boost::variate_generator<boost::mt19937&, boost::uniform_real<> > uni_;
   boost::variate_generator<boost::mt19937&, boost::normal_distribution<> > normal_;
 };
-
 }
 
 #endif
--- src/random_numbers.cpp
+++ src/random_numbers.cpp
@@ -49,9 +49,10 @@ static boost::uint32_t first_seed_ = 0;
 static boost::uint32_t firstSeed(void)
 {
   boost::scoped_ptr<int> mem(new int());
-  first_seed_ = (boost::uint32_t)((boost::posix_time::microsec_clock::universal_time() -
-                            boost::posix_time::ptime(boost::date_time::min_date_time)).total_microseconds() +
-                           (unsigned long long)(mem.get()));
+  first_seed_ = (boost::uint32_t)(
+      (boost::posix_time::microsec_clock::universal_time() - boost::posix_time::ptime(boost::date_time::min_date_time))
+          .total_microseconds() +
+      (unsigned long long)(mem.get()));
   return first_seed_;
 }
 
@@ -63,27 +64,23 @@ static boost::uint32_t nextSeed(void)
   static boost::mutex rngMutex;
   boost::mutex::scoped_lock slock(rngMutex);
   static boost::lagged_fibonacci607 sGen(firstSeed());
-  static boost::uniform_int<>       sDist(1, 1000000000);
+  static boost::uniform_int<> sDist(1, 1000000000);
   static boost::variate_generator<boost::lagged_fibonacci607&, boost::uniform_int<> > s(sGen, sDist);
   boost::uint32_t v = s();
   return v;
 }
 
-
-random_numbers::RandomNumberGenerator::RandomNumberGenerator(void) : generator_(nextSeed()),
-                                                                     uniDist_(0, 1),  
-                                                                     normalDist_(0, 1),
-                                                                     uni_(generator_, uniDist_),
-                                                                     normal_(generator_, normalDist_)
+random_numbers::RandomNumberGenerator::RandomNumberGenerator(void)
+  : generator_(nextSeed())
+  , uniDist_(0, 1)
+  , normalDist_(0, 1)
+  , uni_(generator_, uniDist_)
+  , normal_(generator_, normalDist_)
 {
 }
 
 random_numbers::RandomNumberGenerator::RandomNumberGenerator(boost::uint32_t seed)
-                                                                   : generator_(seed),
-                                                                     uniDist_(0, 1),
-                                                                     normalDist_(0, 1),
-                                                                     uni_(generator_, uniDist_),
-                                                                     normal_(generator_, normalDist_)
+  : generator_(seed), uniDist_(0, 1), normalDist_(0, 1), uni_(generator_, uniDist_), normal_(generator_, normalDist_)
 {
   // Because we manually specified a seed, we need to save it ourselves
   first_seed_ = seed;
@@ -95,7 +92,8 @@ void random_numbers::RandomNumberGenerator::quaternion(double value[4])
 {
   double x0 = uni_();
   double r1 = sqrt(1.0 - x0), r2 = sqrt(x0);
-  double t1 = 2.0 * boost::math::constants::pi<double>() * uni_(), t2 = 2.0 * boost::math::constants::pi<double>() * uni_();
+  double t1 = 2.0 * boost::math::constants::pi<double>() * uni_(),
+         t2 = 2.0 * boost::math::constants::pi<double>() * uni_();
   double c1 = cos(t1), s1 = sin(t1);
   double c2 = cos(t2), s2 = sin(t2);
   value[0] = s1 * r1;
