bfbf77b93be68b6215c06faee16831cd83222448
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2026-06-15 12:40:36 +0200

    Switch to find_package(Boost CONFIG)
    ... to fix cmake policy warning

--- ConfigExtras.cmake
+++ ConfigExtras.cmake
@@ -28,4 +28,4 @@
 
 # Put find_package calls to packages with components so
 # dependent packages can link against component libraries
-find_package(Boost REQUIRED COMPONENTS random)
+find_package(Boost CONFIG REQUIRED COMPONENTS random)
